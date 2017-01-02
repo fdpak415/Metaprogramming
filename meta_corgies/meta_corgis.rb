1d2a148266a29db7c7e1307f0ca7dec175e53d45
Frank Pak <fpak210@gmailc.om>
2017-01-02 12:11:02 -0800

    complete meta_corgis

--- meta_corgies/meta_corgis.rb
+++ meta_corgies/meta_corgis.rb
@@ -1,3 +1,5 @@
+
+
 class SnackBox
   SNACK_BOX_DATA = {
     1 => {
@@ -112,10 +114,22 @@ class MetaCorgiSnacks
 
   def method_missing(name, *args)
     # Your code goes here...
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    name = "#{name.to_s.split('_').map(&:capitalize).join(' ')}"
+    result = "#{name}: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
   end
 
 
   def self.define_snack(name)
     # Your code goes here...
+    define_method(name) do
+      info = @snack_box.send("get_#{name}_info", @box_id)
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      display_name = "#{name.split('_').map(&:capitalize).join(' ')}"
+      result = "#{display_name}: #{info}: #{tastiness}"
+      tastiness > 30 ? "* #{result}" : result
+    end
   end
 end
